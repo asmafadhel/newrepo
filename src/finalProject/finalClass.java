863c4ee6a021d39dec75fd1ff847559ec25f4492
asmafadhel <asma.fadhel1994@gmail.com>
2022-09-28 20:49:01 -0400

    first commit

--- src/finalProject/finalClass.java
+++ src/finalProject/finalClass.java
@@ -3,7 +3,7 @@ package finalProject;
 public class finalClass {
 public static void main(String[] args) {
 	
-	System.out.println("this is the code for  mercedes");
+	System.out.println("this is the code for mercedes");
 	
 	
 }
---
+++ src/finalProject/finalProject.java
@@ -0,0 +1,8 @@
+package finalProject;
+
+public class finalProject {
+	public static void main(String[] args) {
+		System.out.println("this is the code for the audi");
+	}
+
+}
